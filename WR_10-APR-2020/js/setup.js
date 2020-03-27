687d803ca97a7d5e265956fd544bab717d40bacd
mrmariuspop <mr.marius.pop@gmail.com>
2020-03-27 11:05:13 +0200

    host images in cloud

---
+++ WR_10-APR-2020/js/setup.js
@@ -0,0 +1,182 @@
+var slideIndex = 0;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+   plusDivs(1);
+  
+  setTimeout(carousel, 2500);    
+}
+
+
+
+// Modal Image Gallery
+
+function onClick(element) {
+
+  var initialImageSrc = element.src;
+  //display the high resolution image when clicked , NOT the preview as displayed on the main page (..._big extensions)
+  if (initialImageSrc.includes("JUO01n5"))
+  {	  
+	  document.getElementById("img01").src = element.src.replace("JUO01n5","JUO01n5");
+
+  }
+  if (initialImageSrc.includes("https://i.imgur.com/vueWSHj.jpg"))
+  {	  
+	  document.getElementById("img01").src = element.src.replace("https://i.imgur.com/vueWSHj.jpg","https://i.imgur.com/vueWSHj.jpg");
+
+  }
+  if (initialImageSrc.includes("https://i.imgur.com/JEMWPvm.jpg"))
+  {	  
+	  document.getElementById("img01").src = element.src.replace("https://i.imgur.com/JEMWPvm.jpg","https://i.imgur.com/JEMWPvm.jpg");
+
+  }
+  if (initialImageSrc.includes("https://i.imgur.com/AySarWs.jpg"))
+  {	  
+	  document.getElementById("img01").src = element.src.replace("https://i.imgur.com/AySarWs.jpg","https://i.imgur.com/AySarWs.jpg");
+
+  }
+  if (initialImageSrc.includes("https://i.imgur.com/nYoh2Ug.jpg"))
+  {	  
+	  document.getElementById("img01").src = element.src.replace("https://i.imgur.com/nYoh2Ug.jpg","https://i.imgur.com/mCXEdwD.jpg");
+
+  }
+  if (initialImageSrc.includes("https://i.imgur.com/SvPIlS2.jpg"))
+  {	  
+	  document.getElementById("img01").src = element.src.replace("https://i.imgur.com/SvPIlS2.jpg","https://i.imgur.com/sy6tb25.jpg");
+
+  }
+  if (initialImageSrc.includes("https://i.imgur.com/Jzc2z4l.jpg"))
+  {	  
+	  document.getElementById("img01").src = element.src.replace("https://i.imgur.com/Jzc2z4l.jpg","https://i.imgur.com/QV3gwmi.jpg");
+
+  }
+  if (initialImageSrc.includes("https://i.imgur.com/VdwkQss.jpg"))
+  {	  
+	  document.getElementById("img01").src = element.src.replace("https://i.imgur.com/VdwkQss.jpg","https://i.imgur.com/tqfRgWE.jpg");
+
+  }
+  
+
+  document.getElementById("modal01").style.display = "block";
+
+  var captionText = document.getElementById("caption");
+
+  captionText.innerHTML = element.alt;
+
+}
+
+
+
+// Change style of navbar on scroll
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+
+    var navbar = document.getElementById("myNavbar");
+
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+
+    } else {
+
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+
+    }
+
+}
+
+
+
+// Used to toggle the menu on small screens when clicking on the menu button
+
+function toggleFunction() {
+
+    var x = document.getElementById("navDemo");
+
+    if (x.className.indexOf("w3-show") == -1) {
+
+        x.className += " w3-show";
+
+    } else {
+
+        x.className = x.className.replace(" w3-show", "");
+
+    }
+
+}
+
+function validateForm() {
+	console.log("S-a apelat functia");
+  var x = document.forms["myForm"]["Email"].value;
+  console.log(x);
+  if (!x.includes('@')) {
+	  console.log("Am intrat in if");
+    document.forms["myForm"]["Email"].value = 'Invalid Email';
+    document.forms["myForm"]["Email"].style.color = "Red";
+    return false;
+  }
+}
+
+function clicked() {
+	console.log("Am apelat clicked");
+	var x = document.forms["myForm"]["Email"].value;
+	if (x.includes('Invalid')) {
+		console.log('Contine invalid');
+		document.forms["myForm"]["Email"].value = '';
+		document.forms["myForm"]["Email"].style.color = "black";
+	} else {
+		console.log('Nu contine invalid');
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
